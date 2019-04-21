472251db719845cd25df8efae3f30771f17ec9f9
DarkWitchZH <30944446+DarkWitchZH@users.noreply.github.com>
2019-04-22 01:33:40 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+//main.js
+//主函数 
+var roleHarvester = require('role.harvester');
+
+module.exports.loop = function () {
+
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        roleHarvester.run(creep);
+    }
+}
